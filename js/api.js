7135120091482388d1a6e8ef7ac29f2621b2b748
Ingrid M. <ingridzmendoza@gmail.com>
2026-02-14 11:41:27 -0700

    feat: implement 5 day forecast

--- js/api.js
+++ js/api.js
@@ -11,3 +11,15 @@ export async function getWeather(city) {
 
     return await response.json();
 }
+
+export async function getForecast(city) {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric&lang=es`;
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error("Error en el pronóstico");
+    }
+
+    return await response.json();
+}
--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-import { getWeather } from "./api.js";
+import { getWeather, getForecast } from "./api.js";
 
 const form = document.getElementById("searchForm");
 const cityInput = document.getElementById("cityInput");
 const weatherResult = document.getElementById("weatherResult");
+const forecastDiv = document.getElementById("forecast");
 
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
@@ -15,8 +16,12 @@ form.addEventListener("submit", async (e) => {
     }
 
     try {
-        const data = await getWeather(city);
-        showWeather(data);
+        const weatherData = await getWeather(city);
+        showWeather(weatherData);
+
+        const forecastData = await getForecast(city);
+        showForecast(forecastData);
+
     } catch (error) {
         weatherResult.classList.remove("hidden");
         weatherResult.innerHTML = `<p>Error: ${error.message}</p>`;
@@ -27,12 +32,38 @@ function showWeather(data) {
     weatherResult.classList.remove("hidden");
 
     weatherResult.innerHTML = `
-    <h2>${data.name}</h2>
-    <p>Temperatura: ${data.main.temp} °C</p>
-    <p>Clima: ${data.weather[0].description}</p>
-    <img
+        <h2>${data.name}</h2>
+        <p>Temperatura: ${data.main.temp} °C</p>
+        <p>Clima: ${data.weather[0].description}</p>
+        <img
         src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"
         alt="Icono del clima"
-    />
+        />
     `;
 }
+
+function showForecast(data) {
+    forecastDiv.classList.remove("hidden");
+
+    forecastDiv.innerHTML = "<h2>Pronóstico 5 días</h2>";
+
+    // Filtrar solo el pronóstico de las 12:00 PM
+    const dailyForecast = data.list.filter(item =>
+        item.dt_txt.includes("12:00:00")
+    );
+
+    dailyForecast.forEach(day => {
+        const date = day.dt_txt.split(" ")[0];
+        const temp = day.main.temp;
+        const desc = day.weather[0].description;
+        const icon = day.weather[0].icon;
+
+        forecastDiv.innerHTML += `
+        <div class="forecast-day">
+            <p><strong>${date}</strong></p>
+            <img src="https://openweathermap.org/img/wn/${icon}.png" />
+            <p>${temp} °C</p>
+            <p>${desc}</p>
+        </div>
+        `;
+    });}
